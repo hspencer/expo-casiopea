f413d2d7b91d639ad483c092e04924d1fb58d213
Herbert Spencer <hspencer@ead.cl>
2022-07-20 16:07:59 -0400

    adj

--- 027-random-drawing-blob/sketch.js
+++ 027-random-drawing-blob/sketch.js
@@ -1,20 +1,20 @@
 let b = [];
-let num = 7;
+let num = 70;
 
 function setup() {
-  createCanvas(windowWidth - 17, windowHeight - 17);
+  createCanvas(windowWidth, windowHeight);
   b.push(new Blob(random(width), random(height)));
 }
 
 function draw() {
-  blendMode(BLEND);
-  background("#f2edd945");
-  blendMode(MULTIPLY); 
+  //blendMode(BLEND);
+  background("#FFFFFFaa");
+  //blendMode(MULTIPLY); 
   for (let blob of b) {
     blob.live();
   }
 
-  if (b.length < num && random(1) < 0.01) {
+  if (b.length < num && random(1) < 0.0075) {
     b.push(new Blob(random(width), random(height)));
   }
 }
@@ -33,6 +33,8 @@ class Blob {
   }
 
   live() {
+    
+    // determina el primer y último vértice
     let first = this.v[0];
     let last = this.v[this.v.length - 1];
 
@@ -42,27 +44,35 @@ class Blob {
 
       // si ya es muy largo tiene que comenzar a volver
       if (this.v.length >= this.perimeter) {
+        
         let ang = atan2(first.y - last.y, first.x - last.x);
         let diff = ang - this.t;
-        this.t += diff * 0.1 + n;
+        this.t += diff * 0.345 + n;
 
+        let distLast = dist(first.x, first.y, last.x, last.y);
+        let distBefore = dist(first.x, first.y, this.v[this.v.length - 2].x, this.v[this.v.length - 2].y);
+        
         // si los extremos están cerca cierre la figura
-        if (dist(first.x, first.y, last.x, last.y) < 5) {
+        if (distLast < 2) {
           this.open = false;
         }
+        
+        // si se está alejando, que gire en 180º
+        if(distLast > distBefore){
+          this.t += PI;
+        }
+
       } else {
         this.t += n;
       }
-     
-
       // si choca con los bordes
       if (last.x > width || last.x < 0 || last.y < 0 || last.y > height) {
         this.t += PI;
       }
 
       // identidad circular
-      let x = last.x + cos(this.t) * 2;
-      let y = last.y + sin(this.t) * 2;
+      let x = last.x + cos(this.t) * 1;
+      let y = last.y + sin(this.t) * 1;
 
       // agregue un nuevo vértice (sólo si tiene un largo razonable)
       if (this.v.length < this.perimeter * 2.5) {
@@ -71,6 +81,8 @@ class Blob {
     }
 
     if (!this.open) {
+
+      // relleno de color
       noStroke();
       beginShape();
       fill(this.fill);
@@ -78,23 +90,28 @@ class Blob {
         vertex(p.x, p.y);
       }
       endShape();
+
+      // y el trazo que cierra la figura
       stroke(this.strk);
       line(first.x, first.y, last.x, last.y);
     }
 
+    // todos los otros trazos
     for (let i = 1; i < this.v.length; i++) {
       strokeWeight(noise(this.v[i].x / 20, this.v[i].y / 20) * 5);
+      stroke(this.strk);
       line(this.v[i].x, this.v[i].y, this.v[i - 1].x, this.v[i - 1].y);
     }
   }
 }
 
 let fillColors = [
-  "#26465322","#2a9d8f22","#e9c46a66","#f4a26122","#e76f5122"
+  "#56766199", "#987f25c7", "#78858599", "#c93b03bf", "#b05e0cc7", "#0490a9bf"
+  
 ];
 
 let strokeColors = [
-  "#56766166", "#9caea966", "#78858566", "#6f686666", "#38302e44"
+  "#08054dbf","#2a9d8f22","#1a2f32bf","#68860391","#e76f5122"
 ]
 
 function getCol(palete) {
