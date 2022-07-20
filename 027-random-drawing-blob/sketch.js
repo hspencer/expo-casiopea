26384f2e384b4b7d8eb7e90472b7dbc2fa855983
Herbert Spencer <hspencer@ead.cl>
2022-07-20 12:36:40 -0400

    adj

--- 027-random-drawing-blob/sketch.js
+++ 027-random-drawing-blob/sketch.js
@@ -8,7 +8,7 @@ function setup() {
 
 function draw() {
   blendMode(BLEND);
-  background("#FFFFFF55");
+  background("#f2edd945");
   blendMode(MULTIPLY); 
   for (let blob of b) {
     blob.live();
@@ -27,9 +27,9 @@ class Blob {
     this.t = random(TWO_PI);
     this.perimeter = round(random(30, 500));
     this.seed = round(random(50, 100));
-    this.fill = getCol();
-    this.strk = getCol();
-    this.zoom = random(200);
+    this.fill = getCol(fillColors);
+    this.strk = getCol(strokeColors);
+    this.zoom = random(40);
   }
 
   live() {
@@ -41,10 +41,10 @@ class Blob {
       let n = noise(millis() / this.zoom) - 0.5;
 
       // si ya es muy largo tiene que comenzar a volver
-      if (this.v.length > this.perimeter) {
+      if (this.v.length >= this.perimeter) {
         let ang = atan2(first.y - last.y, first.x - last.x);
         let diff = ang - this.t;
-        this.t += diff * 0.2 + n;
+        this.t += diff * 0.1 + n;
 
         // si los extremos están cerca cierre la figura
         if (dist(first.x, first.y, last.x, last.y) < 5) {
@@ -53,7 +53,7 @@ class Blob {
       } else {
         this.t += n;
       }
-      this.t %= TWO_PI;
+     
 
       // si choca con los bordes
       if (last.x > width || last.x < 0 || last.y < 0 || last.y > height) {
@@ -65,13 +65,12 @@ class Blob {
       let y = last.y + sin(this.t) * 2;
 
       // agregue un nuevo vértice (sólo si tiene un largo razonable)
-      if (this.v.length < this.perimeter * 2) {
+      if (this.v.length < this.perimeter * 2.5) {
         this.v.push(createVector(x, y));
       }
     }
 
     if (!this.open) {
-      line(first.x, first.y, last.x, last.y);
       noStroke();
       beginShape();
       fill(this.fill);
@@ -79,9 +78,10 @@ class Blob {
         vertex(p.x, p.y);
       }
       endShape();
+      stroke(this.strk);
+      line(first.x, first.y, last.x, last.y);
     }
 
-    stroke(this.strk);
     for (let i = 1; i < this.v.length; i++) {
       strokeWeight(noise(this.v[i].x / 20, this.v[i].y / 20) * 5);
       line(this.v[i].x, this.v[i].y, this.v[i - 1].x, this.v[i - 1].y);
@@ -89,26 +89,16 @@ class Blob {
   }
 }
 
-let col = [
-  "#a3162166",
-  "#bfdbf766",
-  "#053c5e66",
-  "#1f7a8c66",
-  "#db222a66",
-  "#d8e2dc66",
-  "#ffe5d966",
-  "#ffcad466",
-  "#f4acb766",
-  "#9d818966",
-  "#17161466",
-  "#3a261866",
-  "#75404366",
-  "#9a887366",
-  "#37423d66",
+let fillColors = [
+  "#26465322","#2a9d8f22","#e9c46a66","#f4a26122","#e76f5122"
 ];
 
-function getCol() {
-  return col[floor(random(col.length))];
+let strokeColors = [
+  "#56766166", "#9caea966", "#78858566", "#6f686666", "#38302e44"
+]
+
+function getCol(palete) {
+  return palete[floor(random(palete.length))];
 }
 
 function mousePressed(){
