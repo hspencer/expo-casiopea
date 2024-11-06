413086b424c66f6966ea76551b3c3bc6a760b908
Herbert Spencer <hspencer@ead.cl>
2024-11-07 00:06:24 +1300

    sin rotar 180

--- 049-travesias-40-aniversario/sketch.js
+++ 049-travesias-40-aniversario/sketch.js
@@ -38,7 +38,6 @@ const MARGIN_TOP = 50;      // Margen superior para evitar recortes
 
 // Carga inicial de datos
 function preload() {
-  // Carga los datos de la wiki usando JSONP
   loadJSON(
     "https://wiki.ead.pucv.cl/api.php?action=ask&format=json&query=%5B%5BCategor%C3%ADa%3ATraves%C3%ADa%5D%5D%20%7C%3F%20A%C3%B1o%20%7C%3F%20Posici%C3%B3n%20%7C%3F%20Destino%7Climit%3D9999&utf8=1&formatversion=latest",
     gotData,
@@ -55,8 +54,8 @@ function setup() {
 
   // Creación y configuración del canvas
   createCanvas(windowWidth, CANVAS_HEIGHT, WEBGL);
-  //blendMode(MULTIPLY);
-  frameRate(1);
+  blendMode(MULTIPLY);
+  frameRate(120);
   
   // Configuración de tipografía
   font = loadFont("Alegreya-Regular.ttf");
@@ -132,6 +131,7 @@ function draw() {
     
     // Dibujo de la travesía
     travesia.show();
+    
     currentYearIndex++;
   } else {
     noLoop();
@@ -150,7 +150,6 @@ class Travesia {
     this.size = random(15, 30);
   }
 
-  // Conversión de coordenadas geográficas a coordenadas de pantalla
   latLonToPixel() {
     // Calculamos las dimensiones efectivas del área de dibujo
     const mapHeight = height - MARGIN_TOP;
@@ -158,33 +157,30 @@ class Travesia {
                       Math.abs(LIMITES_SUDAMERICA.maxLat - LIMITES_SUDAMERICA.minLat);
     const mapWidth = mapHeight * proporcion;
 
-    // Transformación de coordenadas con rotación 180°
-    let x = map(this.lon, LIMITES_SUDAMERICA.maxLon, LIMITES_SUDAMERICA.minLon, 
+    // Transformación de coordenadas invertida para rotación 180°
+    let x = map(this.lon, LIMITES_SUDAMERICA.minLon, LIMITES_SUDAMERICA.maxLon,
                 -mapWidth/2, mapWidth/2);
-    let y = map(this.lat, LIMITES_SUDAMERICA.maxLat, LIMITES_SUDAMERICA.minLat, 
+    let y = map(this.lat, LIMITES_SUDAMERICA.maxLat, LIMITES_SUDAMERICA.minLat,
                 -mapHeight/2 + MARGIN_TOP/2, mapHeight/2 + MARGIN_TOP/2);
     
     return createVector(x, y);
   }
 
-  // Visualización de la travesía
   show() {
     let pos = this.latLonToPixel();
 
-    // Debug en consola
     console.log(`Pintando: ${this.name} (${this.year})
       Coordenadas originales: lat ${this.lat}, lon ${this.lon}
       Coordenadas en pantalla: x ${pos.x}, y ${pos.y}`);
 
     push();
     translate(pos.x, pos.y, 0);
+    rotate(PI); // Rotación de 180 grados
 
-    // Dibujo con p5.brush
     brush.stroke(this.color);
     brush.pick("rotring");
     brush.beginStroke("segments", 0, 0);
     
-    // Trazos aleatorios para la mancha
     for (let i = 0; i < random(5, 15); i++) {
       let angle = random(30, 60);
       let length = random(10, 30);
@@ -199,5 +195,5 @@ class Travesia {
 // Manejo de redimensionamiento de ventana
 function windowResized() {
   resizeCanvas(windowWidth, CANVAS_HEIGHT);
-  infoDiv.position(20, CANVAS_HEIGHT - 40); // Reposicionamos el div
+  infoDiv.position(20, CANVAS_HEIGHT - 40);
 }
